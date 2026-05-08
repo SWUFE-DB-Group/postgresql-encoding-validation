c46ab154cf9b7d070fd2128b6bfa4833ba2a6e97
CHEN Zhongpu <chenloveit@gmail.com>
2026-05-08 15:31:14 +0800

    complete GUC for `copy_encoding_validation`

---
+++ DEV.md
@@ -0,0 +1,7 @@
+# ReSync Patch for PostgreSQL
+
+```sql
+SHOW copy_encoding_validation;
+SET copy_encoding_validation = 'native';
+SET copy_encoding_validation = 'read_compatible';
+```
--- src/backend/utils/adt/version.c
+++ src/backend/utils/adt/version.c
@@ -16,9 +16,6 @@
 
 #include "utils/builtins.h"
 
-
-Datum
-pgsql_version(PG_FUNCTION_ARGS)
-{
-	PG_RETURN_TEXT_P(cstring_to_text(PG_VERSION_STR));
+Datum pgsql_version(PG_FUNCTION_ARGS) {
+  PG_RETURN_TEXT_P(cstring_to_text(PG_VERSION_STR " [encoding-valiation-dev]"));
 }
